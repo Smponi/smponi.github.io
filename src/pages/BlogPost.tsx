26c6a5c63b09393c4fd4d5f7c7cf69bdbd172a82
Smponi <Philipp@smponias.de>
2026-01-04 10:56:55 +0100

    feature: Blog added

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -166,7 +166,7 @@ export function BlogPost() {
                 <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
-                Alle Blog-Eintraege
+                Alle Blog-Einträge
               </Link>
             </motion.div>
           </motion.div>
